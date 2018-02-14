4a59bd75c94c82af56cb6daf722fd138097c8775
zmercedes <zmercedes89@gmail.com>
2018-02-14 11:53:55 -0500

    added object spawning with menu

--- HandInteraction.cs
+++ HandInteraction.cs
@@ -13,6 +13,15 @@ public class HandInteraction : MonoBehaviour {
 
 	public float throwForce = 1.5f;
 
+	// swipe
+	public ObjectMenuManager OMM;
+	public float swipeSum;
+	public float touchLast;
+	public float touchCurrent;
+	public float distance;
+	public bool hasSwipedLeft;
+	public bool hasSwipedRight;
+
 	// Use this for initialization
 	void Start () {
 		trackedObj = GetComponent<SteamVR_TrackedObject>();
@@ -21,6 +30,57 @@ public class HandInteraction : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		device = SteamVR_Controller.Input((int)trackedObj.index);
+		if(device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+			touchLast = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+			
+		if(device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad)){
+			touchCurrent = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+			distance = touchCurrent - touchLast;
+			touchLast = touchCurrent;
+			swipeSum += distance;
+
+			if(!hasSwipedRight){
+				if(swipeSum > 0.5f){
+					swipeSum = 0;
+					Swipe(true);
+					hasSwipedRight = true;
+					hasSwipedLeft = false;
+				}
+			}
+
+			if(!hasSwipedLeft){
+				if(swipeSum < -0.5f){
+					swipeSum = 0;
+					Swipe(false);
+					hasSwipedLeft = true;
+					hasSwipedRight = false;
+				}
+			}
+		}
+		if(device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad)){
+			swipeSum = 0;
+			touchCurrent = 0;
+			touchLast = 0;
+			hasSwipedLeft = false;
+			hasSwipedRight = false;
+		}
+
+		if(device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+			SpawnObject();
+	}
+
+	void SpawnObject(){
+		OMM.SpawnCurrentObject();
+	}
+
+	void Swipe(bool direction){
+		if(direction){
+			OMM.MenuRight();
+			Debug.Log("Swiped right!");
+		} else {
+			OMM.MenuLeft();
+			Debug.Log("Swiped left!");
+		}
 	}
 
 	void OnTriggerStay(Collider col){
